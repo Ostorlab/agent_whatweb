93ce18ac6e2d9404d17a09391252d9dd53a554be
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-07-22 11:15:34 +0100

    Update and add another unit test.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,31 @@
 """Pytest fixture for the whatweb agent."""
 import pytest
+import json
 
 from ostorlab.agent import definitions as agent_definitions
 from ostorlab.runtimes import definitions as runtime_definitions
+from ostorlab.utils import defintions
+from ostorlab.agent import message as m
+
 from agent import whatweb_agent
+
+
+@pytest.fixture
+def domain_msg():
+    """Creates a dummy message of type v3.asset.domain_name for testing purposes."""
+    input_selector = 'v3.asset.domain_name'
+    input_data = {'name': 'ostorlab.co'}
+    message = m.Message.from_data(selector=input_selector, data=input_data)
+    return message
+
+
+@pytest.fixture
+def link_msg():
+    """Creates a dummy message of type v3.asset.link for testing purposes."""
+    input_selector = 'v3.asset.link'
+    input_data = {'url': 'http://ostorlab.co', 'method': 'GET'}
+    message = m.Message.from_data(selector=input_selector, data=input_data)
+    return message
 
 
 @pytest.fixture
@@ -11,5 +33,14 @@ def whatweb_test_agent():
     """Creates a dummy agent for the WhatWeb Agent.
     """
     agent_definition = agent_definitions.AgentDefinition(name='whatweb')
-    agent_settings = runtime_definitions.AgentSettings(key='whatweb')
+    agent_settings = runtime_definitions.AgentSettings(
+        key='whatweb',
+        args=[
+            defintions.Arg(name='schema',
+                           type='string',
+                           value=json.dumps('https').encode()),
+            defintions.Arg(name='port',
+                           type='number',
+                           value=json.dumps(443).encode())
+        ])
     return whatweb_agent.AgentWhatWeb(agent_definition, agent_settings)
--- tests/whatweb_test.py
+++ tests/whatweb_test.py
@@ -2,28 +2,25 @@
 import pathlib
 import tempfile
 
-from ostorlab.agent import message as msg
 
-
-def testWhatWebAgent_withAllChecksEnabled_emitsFingerprints(whatweb_test_agent, mocker):
+def testWhatWebAgent_withDomainMsgAndAllChecksEnabled_emitsFingerprints(agent_mock, whatweb_test_agent, domain_msg, mocker):
     """Test the whatweb agent with a given target address. The tests mocks the call to WhatWeb binary
-    and validates the parsing and sending the findings to the queue
+    and validates the parsing and sending the findings to the queue.
     """
-
-    input_selector = 'v3.asset.domain_name'
-    input_data = {'name': 'ostorlab.co'}
+    del agent_mock
     detail = 'Found library `Google-Analytics`, version `Universal`, of type `BACKEND_COMPONENT` in domain ' \
              '`ostorlab.co`'
-    output_selector = 'v3.fingerprint.domain_name.library'
+    output_selector = 'v3.fingerprint.domain_name.service.library'
     output_data = {
-        'domain_name': 'ostorlab.co',
+        'name': 'ostorlab.co',
+        'port': 443,
+        'schema': 'https',
         'library_name': 'Google-Analytics',
         'library_version': 'Universal',
         'library_type': 'BACKEND_COMPONENT',
         'detail': detail
     }
 
-    message = msg.Message.from_data(selector=input_selector, data=input_data)
     mocker.patch('subprocess.run', return_value=None)
     mock_emit = mocker.patch('agent.whatweb_agent.AgentWhatWeb.emit', return_value=None)
     with tempfile.TemporaryFile() as fp:
@@ -31,5 +28,36 @@ def testWhatWebAgent_withAllChecksEnabled_emitsFingerprints(whatweb_test_agent, 
         with open(f'{pathlib.Path(__file__).parent}/output.json', 'rb') as op:
             fp.write(op.read())
             fp.seek(0)
-            whatweb_test_agent.process(message)
+            whatweb_test_agent.process(domain_msg)
+            mock_emit.assert_any_call(selector=output_selector, data=output_data)
+
+
+def testWhatWebAgent_withLinkMsgAndAllChecksEnabled_emitsFingerprints(agent_mock, whatweb_test_agent, link_msg, mocker):
+    """Test the whatweb agent with a given target address. The tests mocks the call to WhatWeb binary
+    and validates the parsing and sending the findings to the queue.
+    The test also ensures the correct compute of the port and schema from the target link.
+    """
+    del agent_mock
+
+    detail = 'Found library `Google-Analytics`, version `Universal`, of type `BACKEND_COMPONENT` in domain ' \
+             '`ostorlab.co`'
+    output_selector = 'v3.fingerprint.domain_name.service.library'
+    output_data = {
+        'name': 'ostorlab.co',
+        'port': 80,
+        'schema': 'http',
+        'library_name': 'Google-Analytics',
+        'library_version': 'Universal',
+        'library_type': 'BACKEND_COMPONENT',
+        'detail': detail
+    }
+
+    mocker.patch('subprocess.run', return_value=None)
+    mock_emit = mocker.patch('agent.whatweb_agent.AgentWhatWeb.emit', return_value=None)
+    with tempfile.TemporaryFile() as fp:
+        mocker.patch('tempfile.NamedTemporaryFile', return_value=fp)
+        with open(f'{pathlib.Path(__file__).parent}/output.json', 'rb') as op:
+            fp.write(op.read())
+            fp.seek(0)
+            whatweb_test_agent.process(link_msg)
             mock_emit.assert_any_call(selector=output_selector, data=output_data)
