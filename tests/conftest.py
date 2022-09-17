1f665b6b742268d6302f1eab3172cd6497b82a4c
amine <amine.mesbahi@ostorlab.dev>
2022-09-17 12:58:51 +0200

    Check processed message outside target processing

--- tests/conftest.py
+++ tests/conftest.py
@@ -61,7 +61,7 @@ def ip_msg_with_port_and_schema() -> m.Message:
 def ip_msg_with_port_schema_mask() -> m.Message:
     """Creates a dummy message of type v3.asset.ip.v4.port.service for testing purposes."""
     input_selector = 'v3.asset.ip.v4.port.service'
-    input_data = {'host': '192.168.0.0', 'port': 80, 'mask': '31', 'protocol': 'http'}
+    input_data = {'host': '192.168.0.0', 'port': 80, 'mask': '32', 'protocol': 'http'}
     message = m.Message.from_data(selector=input_selector, data=input_data)
     return message
 
