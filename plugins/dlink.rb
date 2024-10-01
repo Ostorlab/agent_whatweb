7b9e3164a40731fb906e69bc1351489682715f6b
nmasdoufi-ol <noureddine.masdoufi@ostorlab.dev>
2024-10-01 13:29:04 +0100

    Add fingerprint for D-Link

---
+++ plugins/dlink.rb
@@ -0,0 +1,17 @@
+Plugin.define do
+  name "D-Link"
+  authors [
+    "Ostorlab",
+  ]
+  version "0.1"
+  description "D-Link DIR-820L is a wireless router providing internet access and networking capabilities."
+  website "http://www.dlink.com/"
+
+  matches [
+    {
+      :search => "body",
+      :regexp => /<a href="http:\/\/www.dlink.com\/us\/en\/support">/,
+      :name => "D-Link Support Link"
+    },
+  ]
+end
