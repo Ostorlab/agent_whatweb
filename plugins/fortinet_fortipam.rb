aa11b82163724e4c20d500fa7d604bac0e125e17
nmasdoufi-ol <noureddine.masdoufi@ostorlab.dev>
2024-10-14 11:15:54 +0100

    Update FortiPAM fingerprint.

--- plugins/fortinet_fortipam.rb
+++ plugins/fortinet_fortipam.rb
@@ -3,15 +3,18 @@ Plugin.define do
   authors [
     "Ostorlab",
   ]
-  version "0.1"
-  description "FortiPAM provides tightly controlled privileged access to the most sensitive resources within an organization. "
+  version "0.2"
+  description "FortiPAM provides tightly controlled privileged access to the most sensitive resources within an organization."
   website "https://www.fortinet.com/products/fortipam"
 
   matches [
     {
-      :search => "body",
-      :regexp => /FortiPAM/,
-      :name => "FortiPAM Body Content"
+      :regexp => /<img[^>]*src="[^"]*\/FortiPAM\.png"[^>]*>/,
+      :name => "FortiPAM Image"
+    },
+    {
+      :regexp => /<img[^>]*class="navbar-brand"[^>]*src="[^"]*\/FortiPAM\.png"[^>]*>/,
+      :name => "FortiPAM Navbar Image"
     }
   ]
 end
