8933bee3896aaa26591cb1c6def515d5c3cb0d4a
nmasdoufi-ol <noureddine.masdoufi@ostorlab.dev>
2024-11-04 10:52:26 +0100

    Add Fingerprint for PTZOptics Cameras.

---
+++ plugins/ptz_optics_camera.rb
@@ -0,0 +1,17 @@
+Plugin.define do
+  name "ValueHD PTZ Camera"
+  authors [
+    "Ostorlab"
+  ]
+  version "0.1"
+  description "A PTZOptics camera offers a flexible solution for recording and live streaming events due to its pan, tilt, and zoom abilities and high-quality image."
+  website "https://www.ptzoptics.com/"
+
+  matches [
+    {
+      :search => "body",
+      :regexp => /<script type="text\/javascript" src="dist\/ptzomidi_v2\.js"><\/script>/,
+      :name => "PTZOptics JavaScript file mention in the body of the page"
+    }
+  ]
+end
