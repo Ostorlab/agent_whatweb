f93144c052dde2ac76d907e8aeec71260ec2b0d5
nmasdoufi-ol <noureddine.masdoufi@ostorlab.dev>
2024-11-29 10:33:15 +0100

    Add fingerprint for D-Link DNS ShareCenter

---
+++ plugins/sharecenter_dlink.rb
@@ -0,0 +1,17 @@
+Plugin.define do
+  name "D-Link DNS ShareCenter"
+  authors [
+    "Ostorlab",
+  ]
+  version "0.1"
+  description "This ShareCenter™ Cloud Storage device enables you to share documents and media content such as photos, music and videos on a home network or over the Internet."
+  website " https://www.dlink.com/"
+
+  matches [
+    {
+      :search => "body",
+      :regexp => /In order to access the ShareCenter/,
+      :name => "ShareCenter body Tag"
+    },
+  ]
+end
