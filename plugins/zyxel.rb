d5ed7c1cf749342a541d4f446fc3458220b87605
pirahnasa <saad@ostorlab.dev>
2024-12-02 17:15:58 +0100

    Adding ZYXEL fingerprint

---
+++ plugins/zyxel.rb
@@ -0,0 +1,17 @@
+Plugin.define do
+  name "Zyxel Devices"
+  authors [
+    "Ostorlab",
+  ]
+  version "0.1"
+  description "Zyxel provides a wide range of networking solutions, including Unified Security Gateways (USG) and USG FLEX series devices."
+  website "https://www.zyxel.com/"
+
+  matches [
+    {
+      :search => "body",
+      :regexp => /<title>USG\d+<\/title>|<title>USG FLEX \d+<\/title>/i,
+      :name => "Zyxel USG or USG FLEX Title Match"
+    },
+  ]
+end
