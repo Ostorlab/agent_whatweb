69c077afff8f319673e7e34444d02d096739e8ff
nmasdoufi-ol <noureddine.masdoufi@ostorlab.dev>
2025-05-02 16:17:30 +0100

    feature: Add Fingerprint for Craft CMS

---
+++ plugins/craft_cms.rb
@@ -0,0 +1,28 @@
+Plugin.define do
+  name "Craft CMS"
+  authors [
+    "skeltavik",
+    "Nour Eddine Masdoufi <your-email@example.com>", # Optional, add if desired
+  ]
+  version "0.1"
+  description "Craft CMS is a flexible, user-friendly CMS for creating custom digital experiences on the web."
+  website "https://craftcms.com/"
+
+  matches [
+    {
+      :search => "headers[x-powered-by]",
+      :regexp => /Craft CMS/,
+      :name => "X-Powered-By header contains Craft CMS"
+    },
+    {
+      :search => "headers[set-cookie]",
+      :regexp => /(Craft|CRAFT)/,
+      :name => "Set-Cookie header contains Craft or CRAFT"
+    },
+    {
+      :search => "body",
+      :regexp => /CRAFT_CSRF_TOKEN/i,
+      :name => "Body contains CRAFT_CSRF_TOKEN"
+    },
+  ]
+end
