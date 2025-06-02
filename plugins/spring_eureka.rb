afbb17362beda47edbf71da0a43074f8c8330798
nmasdoufi-ol <noureddine.masdoufi@ostorlab.dev>
2025-06-02 11:51:03 +0100

    feature: Add Fingerprint for Spring Eureka.

---
+++ plugins/spring_eureka.rb
@@ -0,0 +1,18 @@
+Plugin.define do
+  name "Spring Eureka"
+  authors [
+    "Ostorlab",
+  ]
+  version "0.1"
+  description "Eureka refers to Spring Cloud Netflix Eureka, a service discovery tool. It is part of the Spring Cloud Netflix stack, designed to facilitate the management of microservices in a distributed system.
+"
+  website "https://cloud.spring.io/spring-cloud-netflix/reference/html/"
+
+  matches [
+    {
+      :search => "title",
+      :regexp => /<title>Sophos<\/title>/,
+      :name => "Sophos Title Tag"
+    },
+  ]
+end
